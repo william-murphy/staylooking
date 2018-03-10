90fcacd5c5f0f484e12b80c739e7f776720d9910
william-murphy <wpm.business@gmail.com>
2018-03-10 11:25:05 -0500

    updated database connection
    added updated db connection info for th e aws rds database

--- ROOT_DB_CONNECT.php
+++ ROOT_DB_CONNECT.php
@@ -1,10 +1,10 @@
 <?php
 
     /*-----! Store the necessary parameters !-----*/
-    $dbServer = "localhost";    //Name of server containing database
-    $dbUsername = "root";       //Username of database
-    $dbPassword = "RqXwrVLmhPPNZZyT5dJNTL7e";           //Password of database
-    $dbName = "root_db";        //Name of database
+    $dbServer =  "staylooking-db.cuutdsnifhtv.us-east-1.rds.amazonaws.com";    //Name of server containing database
+    $dbUsername = "root1";       //Username of database
+    $dbPassword = "tonkaballs17";           //Password of database
+    $dbName = "staylooking";        //Name of database
 
     //Connect to the database
     $connect = mysqli_connect($dbServer, $dbUsername, $dbPassword, $dbName);
